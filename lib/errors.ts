9cd4ecc16d5748434d9ed4cce92eeecb32bca709
asifzulfiqar8 <asifzulfiqar43@gmail.com>
2025-07-09 02:32:06 +0500

    feat: custom error class is added

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,1 +1,12 @@
-export class AppError extends Error {}
+export class AppError extends Error {
+    public readonly statusCode: number;
+    public readonly isOperational: boolean;
+    public readonly errorCode?: string;
+
+    constructor(statusCode:number, message: string, isOperational: boolean, errorCode: string) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = isOperational;
+        this.errorCode = errorCode;
+    }
+}
